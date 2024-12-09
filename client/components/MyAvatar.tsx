8d8256095aae988cc27c8f49d47321802b7855fe
varcharli <varchar@qq.com>
2024-12-09 19:24:14 +0800

    fixed avatar loading

--- client/components/MyAvatar.tsx
+++ client/components/MyAvatar.tsx
@@ -11,8 +11,10 @@ const MyAvatar: React.FC<MyAvatarProps> = ({ src, title }) => {
     console.log('src:', src);
     src = src || nullImage;
     return <div className="flex flex-col items-center justify-center ">
-        <div className={`w-[100px] h-[100px] rounded-full bg-slate-200 border-1 border-slate-300`} >
-            <Image src={src} alt="" className="rounded-full w-[100px] h-[100px] " referrerPolicy='no-referrer' style={{ objectFit: 'cover' }} />
+        <div>
+            <Image src={src} alt="" className="w-[100px] h-[100px] border-1 border-gray-500 "
+                radius="full"
+                referrerPolicy='no-referrer' style={{ objectFit: 'cover' }} />
         </div>
         {title && <h2 className={`w-[100px] text-center`}>{title}</h2>}
     </div>;
